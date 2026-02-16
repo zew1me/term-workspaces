9f4c66f75adbd35a830b24ee850ecc0e48ff1f7f
Nigel Stuke <zew1me>
2026-02-15 16:49:44 -0800

    feat(tasks): add task alias row query type

---
+++ internal/tasks/query.go
@@ -0,0 +1,14 @@
+package tasks
+
+import "time"
+
+type TaskAliasRow struct {
+	TaskID     string
+	AliasType  AliasType
+	AliasValue string
+	Repo       string
+	Branch     string
+	PRNumber   int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
